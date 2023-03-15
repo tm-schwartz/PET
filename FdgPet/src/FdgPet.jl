a86301b941884e10a5aa9e742a1a96f9095d0d6b
tm-schwartz <tm.sc@protonmail.com>
2023-03-15 18:55:34 -0500

    added file extension check and json relocation

--- FdgPet/src/FdgPet.jl
+++ FdgPet/src/FdgPet.jl
@@ -6,12 +6,11 @@ function dcm_add_ext(dir)
     endswithdcm = endswith(".dcm")
     filetype = "DICOM medical imaging data"
     for dicom in readdir(dir)
-        # assume unix based...
         path = joinpath(dir, dicom)
         command = `file -b $path`
         output = readlines(Cmd(command))[1]
         if !endswithdcm(dicom) && output == filetype
-            mv(dicom, string(dicom, ".dcm"))
+            mv(path, string(path, ".dcm"))
         end
     end
 end
@@ -32,22 +31,31 @@ function dcm_dir_to_series(dir)
     return series_dict
 end
 
-
 function process_data_dir(data_dir)
-    studies = filter(isdir,  readdir(data_dir))
+    contents = readdir(data_dir)
+    studies = filter(d -> isdir(joinpath(data_dir, d)), contents)
+    println(studies)
     for study in studies
         study_path = joinpath(data_dir, study)
         series_dict = dcm_dir_to_series(study_path)
+        println(study_path)
         for (seriespath, dicompath) in series_dict
             absolute_seriespath = joinpath(data_dir, seriespath)
-            println("Creating $absolute_seriespath")
             mkpath(absolute_seriespath)
+            println(absolute_seriespath)
             for dicom in dicompath
                 fn = splitdir(dicom)[2]
                 original_path = joinpath(data_dir, study, fn)
                 new_path = joinpath(data_dir, seriespath, fn)
-                mv(joinpath(data_dir, study, fn), joinpath(data_dir, seriespath, fn))
+                mv(original_path, new_path)
+                json_file = string(original_path, ".json")
+                println(json_file)
+                if isfile(json_file)
+                    new_json_path = string(new_path, ".json")
+                    mv(json_file, new_json_path)
+                end
             end
+            dcm_add_ext(absolute_seriespath)
         end
     end
 end
