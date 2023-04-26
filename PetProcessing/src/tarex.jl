5026e2f11ec6a68b583ba168b8c355556b7baaac
tm-schwartz <tm.sc@protonmail.com>
2023-04-26 16:24:49 -0500

    switch to zcat

--- PetProcessing/src/tarex.jl
+++ PetProcessing/src/tarex.jl
@@ -1,6 +1,6 @@
 using Tar
 
 function tarextractor(pattern, tarball, path)
-    toextract = first(filter(hdr->occursin(pattern, hdr.path), Tar.list(`gzcat $tarball`))).path       
-    Tar.extract(x->(x.path == toextract), `gzcat $tarball`, path)
+    toextract = first(filter(hdr->occursin(pattern, hdr.path), Tar.list(`zcat $tarball`))).path       
+    Tar.extract(x->(x.path == toextract), `zcat $tarball`, path)
 end
