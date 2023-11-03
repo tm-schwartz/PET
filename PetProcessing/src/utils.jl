86b88e278ce64aff67d23a2c40ea202cf897a73b
tm-schwartz <tm.sc@protonmail.com>
2023-11-03 17:33:40 -0500

    initial suv pipeline

--- PetProcessing/src/utils.jl
+++ PetProcessing/src/utils.jl
@@ -5,6 +5,10 @@ using JSON3
 using Chain
 using Glob
 using Dates
+using PyCall
+using NIfTI
+
+const KGTOGRAMS = 1000
 
 function generatejsonzippath(path)
     # mrn/studyuid/seriesdescription imagetype stationname/
@@ -26,7 +30,7 @@ function checkforkeyspresent(keystocheck, path)
 end
 
 function editjsonsidecar(skipexistingkeys=false)
-    sidecar = readlines(ENV["SIDECAR_LIST"])[parse(Int,ENV["SLURM_ARRAY_TASK_ID"])]
+    sidecar = readlines(ENV["SIDECAR_LIST"])[parse(Int, ENV["SLURM_ARRAY_TASK_ID"])]
     keystoadd = readlines("derivatives/additionalheaderkeys.txt")
     nkeys = length(keystoadd)
 
@@ -55,18 +59,31 @@ function editjsonsidecar(skipexistingkeys=false)
     open(sidecar, "w") do io
         JSON3.pretty(io, merge(sidecardict, Dict(Symbol(k) => coalesce(v, "missing") for (k, v) in zip(keystoadd, headervalues))))
     end
-    
+
     indxmissing = BitArray(ismissing.(headervalues))
-    
+
     if (m = sum(indxmissing)) > 0
         return "Added $(nkeys - m)/$nkeys to $sidecar. Missing $(keystoadd[indxmissing])"
     end
-    
+
     return "Added all keys to sidecar:$sidecar"
 end
 
-function getsuvbwscalefactor(path)
-    json = JSON3.read(path)
+function smoothvolume(inputvolume, outdir)
+    bn = basename(inputvolume)
+    outfile = joinpath(outdir, replace(bn, "pet" => "8mm_pet"))
+    badgerfsl = `/data/h_vmac/vmac_imaging/fsl_v6.0.5.1.sif`
+    cmd = `fslmaths $inputvolume -s 3.4 $outfile`
+    if isfile(string(badgerfsl))
+        run(`singularity exec $badgerfsl $cmd`)
+    else
+        run(cmd)
+    end
+    return outfile
+end
+
+function getsuvbwscalefactor(sidecarpath)
+    json = (JSON3.read ∘ read)(sidecarpath)
     suvbwscalefactor = let
         seriestime = Time(json.SeriesTime, dateformat"HHMMSS")
         radiopharmaceuticalstarttime = Time(split(json.RadiopharmaceuticalStartTime, '.') |> first, dateformat"HHMMSS")
@@ -78,6 +95,152 @@ function getsuvbwscalefactor(path)
         weightkg * KGTOGRAMS / decaydose
     end
     return suvbwscalefactor
+end
+
+function computesuvvolume(inputvolume, suvscalefactor, suffix)
+    pet = niread(inputvolume)
+    outpath = replace(inputvolume, suffix)
+    niwrite(outpath, NIVolume(pet.header, pet.extensions, pet * suvscalefactor))
+    return outpath
+end
+
+function setsformqform(inputvolume)
+    nii = niread(inputvolume)
+    if nii.header.sform_code != 4 || nii.header.qform_code != 4
+        nii.header.sform_code = 4
+        nii.header.qform_code = 4
+        niwrite(inputvolume, nii)
+    end
+end
+
+function loadsidecar(sidecar)
+    return JSON3.read(sidecar)
+end
+
+function robustfov(inputvolume, outdir)
+    bn = basename(inputvolume)
+    outfile = joinpath(outdir, replace(bn, "pet" => "cropped_pet"))
+    if occursin(r"HN"i, bn)
+        cp(inputvolume, outfile)
+    else
+        badgerfsl = `/data/h_vmac/vmac_imaging/fsl_v6.0.5.1.sif`
+        cmd = `robustfov -i $inputvolume -r $outfile`
+        if isfile(string(badgerfsl))
+            run(`singularity exec $badgerfsl $cmd`)
+        else
+            run(cmd)
+        end
+    end
+    return outfile
+end
+
+function skullstrip(inputvolume, outdir, suffix)
+    bn = basename(inputvolume)
+    outfile = joinpath(outdir, replace(bn, suffix))
+    tivpath = joinpath(outdir, replace(outfile, "nii.gz" => "TIV.csv"))
+    deepbet = pyimport("deepbet")
+    deepbet.run_bet(PyObject([inputvolume]), PyObject([outfile]), tiv_paths=PyObject([tivpath]), threshold=0.9, n_dilate=-2, no_gpu=true)
+    return outfile
+end
+
+py"""
+import SimpleITK as sitk
+
+
+def register(
+    fixed_image_path, moving_image_path, final_moving_image, final_transform_path
+):
+    fixed_image = sitk.ReadImage(fixed_image_path, sitk.sitkFloat32)
+    moving_image = sitk.ReadImage(moving_image_path, sitk.sitkFloat32)
+
+    initial_transform = sitk.CenteredTransformInitializer(
+        fixed_image,
+        moving_image,
+        sitk.Euler3DTransform(),
+        sitk.CenteredTransformInitializerFilter.GEOMETRY,
+    )
+
+    registration_method = sitk.ImageRegistrationMethod()
+
+    # Similarity metric settings.
+    registration_method.SetMetricAsMattesMutualInformation(numberOfHistogramBins=50)
+    registration_method.SetMetricSamplingStrategy(registration_method.RANDOM)
+    registration_method.SetMetricSamplingPercentage(0.01)
+
+    registration_method.SetInterpolator(sitk.sitkLinear)
+
+    # Optimizer settings.
+    registration_method.SetOptimizerAsGradientDescent(
+        learningRate=1.0,
+        numberOfIterations=100,
+        convergenceMinimumValue=1e-6,
+        convergenceWindowSize=10,
+    )
+    registration_method.SetOptimizerScalesFromPhysicalShift()
+
+    # Setup for the multi-resolution framework.
+    registration_method.SetShrinkFactorsPerLevel(shrinkFactors=[4, 2, 1])
+    registration_method.SetSmoothingSigmasPerLevel(smoothingSigmas=[2, 1, 0])
+    registration_method.SmoothingSigmasAreSpecifiedInPhysicalUnitsOn()
+
+    registration_method.SetInitialTransform(initial_transform)
+
+    final_transform = registration_method.Execute(
+        sitk.Cast(fixed_image, sitk.sitkFloat32),
+        sitk.Cast(moving_image, sitk.sitkFloat32),
+    )
+    if final_transform_path != None:
+        sitk.WriteTransform(final_transform, final_transform_path)
+    resampled_moving = sitk.Resample(
+        moving_image,
+        fixed_image,
+        final_transform,
+        sitk.sitkLinear,
+        0.0,
+        moving_image.GetPixelID(),
+    )
+
+    sitk.WriteImage(resampled_moving, final_moving_image)
+
+
+def resample_moving(
+    fixed_image_path, moving_image_path, final_moving_image, resample_with_transform
+):
+    fixed_image = sitk.ReadImage(fixed_image_path, sitk.sitkFloat32)
+    moving_image = sitk.ReadImage(moving_image_path, sitk.sitkFloat32)
+    transform = sitk.ReadTransform(resample_with_transform)
+    # apply transform
+    resampled_moving = sitk.Resample(
+        moving_image,
+        fixed_image,
+        transform,
+        sitk.sitkLinear,
+        0.0,
+        moving_image.GetPixelID(),
+    )
+
+    sitk.WriteImage(resampled_moving, final_moving_image)
+"""
+
+function register(fixedvolumepath, movingvolumepath, outdir, suffix, tfm=false)
+    bn = basename(movingvolumepath)
+    finalmovingvolumepath = joinpath(outdir, replace(bn, suffix))
+    if tfm
+        finaltransformpath = joinpath(outdir, replace(finalmovingvolumepath, "nii.gz" => "tfm"))
+        py"register"(fixedvolumepath, movingvolumepath, finalmovingvolumepath, finaltransformpath)
+    else
+        py"register"(fixedvolumepath, movingvolumepath, finalmovingvolumepath, PyObject(nothing))
+    end
+    setsformqform(finalmovingvolumepath)
+    return finalmovingvolumepath
+end
+
+function resample(fixedvolumepath, movingvolumepath, outdir, suffix)
+    bn = basename(movingvolumepath)
+    finalmovingvolumepath = joinpath(outdir, replace(bn, suffix))
+    resamplewithtransform = joinpath(outdir, replace(bn, "nii.gz" => "tfm"))
+    py"resample_moving"(fixedvolumepath, movingvolumepath, finalmovingvolumepath, resamplewithtransform)
+    setsformqform(finalmovingvolumepath)
 end
 
 
