f158605593788c7e3bc2201ef26e22c8adbbe6a1
tm-schwartz <tm.sc@protonmail.com>
2024-06-28 21:12:07 -0500

    clean

--- PetProcessing/src/jsoncode.sh
+++
@@ -1,6 +0,0 @@
-# sub-dddd/
-#         - sub-dddd_sdit-*_pet.json
-
-dest=$(echo $1 | rg -o --pcre2 'sub-\d{6,8}.*(?=\/data)')
-echo "moving to $dest"
-mv $1 $dest/
--- PetProcessing/src/tartozip.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/zsh
-mrn=$(echo $1 | tr -d ".tar.gz")
-tar -xzf $1 data/h_vmac/koran/FDG_PET/$mrn --strip-components=4
-find $mrn/*/*/*  ! \( -name "*.json" -o -name "*.png" -o -name "*.dcm" \) -type "f" -exec mv {} {}.dcm \;
-zip -rTm $mrn.zip $mrn
